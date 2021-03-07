ae700d88959844c81b5e0a86a533a37b7f746ec3
Daniel Amarante <danz82158@gmail.com>
2021-03-06 23:58:22 -0300

    Update Dnf

---
+++ programa1.py
@@ -0,0 +1,29 @@
+#################################################################
+#Iniciando Estudos com Python.
+#Autor Daniel Amarante
+#Data 17/10/2020.
+#################################################################
+#
+#Introdução ao estudo com Python no Linux
+
+#!/usr/bin/env/ python3
+soma = 5 + 3
+print (soma) 
+
+a = 5
+b = 3
+print("Multiplicacao")
+print (a * b) 
+
+
+print("Divisão")
+    
+print (a / b) 
+
+print("Potenciacao")
+print (a ** b)
+print("Resto da Divisao")
+print (a % b)
+print("\nResultado Inteiro")
+print (a // b)
+
